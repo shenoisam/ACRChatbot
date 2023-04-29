276664b00c087e683cd7031146235bd2be26b1fb
Sam  Shenoi <samshenoi@Sams-MacBook-Pro.local>
2023-04-29 10:22:07 -0500

    Added LLMModel

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import openai
 import os
+from dotenv import load_dotenv
+load_dotenv()
+from LLModel import Model
 
-# Replace YOUR_API_KEY with your OpenAI API key
-openai.api_key = os.environ.get("API_KEY")
+
+
+
+model_engine='gpt-3.5-turbo'
 
 prompt = "Write a blog on ChatGPT"
 
 # Set the maximum number of tokens to generate in the response
 max_tokens = 1024
 
-# Generate a response
-completion = openai.Completion.create(
-    engine=model_engine,
-    prompt=prompt,
-    max_tokens=max_tokens,
-    temperature=0.5,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-)
+def main():
+    Model().generateIndex()
+    query = "How do we eat a potato"
+    results = Model().getIndex().similarity_search(query)
+    print(results[0].page_content)
 
-# Print the response
-print(completion.choices[0].text)
+if __name__ =="__main__":
+    main()
